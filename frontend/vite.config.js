96a4f0024db88cb66389100330bdc00129432e5b
Umedjon Sharipov <umedsharipov0005@gmail.com>
2025-11-24 16:38:37 +0500

    add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,9 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
 export default defineConfig({
   plugins: [react()],
-  base: './',   
+  base: './',
   server: {
     port: 3000,
     host: true,
